b7d6381072e9be602eb68fa3f223645e7f65ea05
lwolfe <lwolfe@fredhutch.org>
2021-09-01 16:13:05 -0700

    build out ui to include reactive sample table, and differential expression results table

--- app.R
+++ app.R
@@ -2,6 +2,11 @@
 # load libraries
 library(shiny)
 library(tidyverse)
+library(DESeq2)
+library(DT)
+
+# source scripts
+source("./helpers.R")
 
 # read in data
 data <- read.table("~/Documents/work/dataCore/shiny/diffEx/data/counts.csv", sep = ",", header = TRUE)
@@ -13,15 +18,14 @@ counts_candidate_f <- function(x) {
     return(FALSE)
 }
 
-info_candidate_f <- function(x) {
+gene_candidate_f <- function(x) {
     if (is.character(data[[x]])) {
         return(TRUE)}
     return(FALSE)
 }
 
 counts_cols <- names(data)[sapply(names(data), counts_candidate_f)]
-info_cols <- names(data)[sapply(names(data), info_candidate_f)]
-
+gene_cols <- names(data)[sapply(names(data), gene_candidate_f)]
 
 # UI -----
 ui <- fluidPage(
@@ -38,9 +42,18 @@ ui <- fluidPage(
                uiOutput("condition2_selector"),
                selectInput("de_package",
                            "Select preferred differential analysis package:",
-                           choices = c("edgeR", "DESeq2", "limma/voom"))
-               
-        )
+                           choices = c("edgeR", "DESeq2", "limma/voom")),
+               br(),
+               actionButton("apply", "Apply")),
+        column(width = 8,
+               h4("Sample Table"),
+               dataTableOutput("sample_table"),
+               br(),
+               hr(),
+               h4("Differential Expression Results"),
+               em("Select replicates, name conditions, select desired analysis method, and click apply"),
+               br(),
+               dataTableOutput("deseq_res_table"))
     ) # end fluidRow
 ) # end fluidPage
 
@@ -50,7 +63,7 @@ server <- function(input, output) {
     # select condition colnames -----
     # condition 1
     output$condition1_selector <- renderUI({
-        selectInput("select_condition1",
+        selectInput("condition1_selected",
                     "Select replicates: ",
                     counts_cols,
                     multiple = TRUE,
@@ -58,11 +71,40 @@ server <- function(input, output) {
     })
     # condition 2
     output$condition2_selector <- renderUI({
-        selectInput("select_condition2",
+        selectInput("condition2_selected",
                     "Select replicates: ",
                     counts_cols,
                     multiple = TRUE,
                     selectize= TRUE)
+    })
+    
+    # Deseq2 differential expression -----
+    reactive_sample_table <- reactive({
+        createSampleMatrix(condition1_name = input$condition1_name,
+                           condition2_name = input$condition2_name,
+                           condition1_selected = input$condition1_selected,
+                           condition2_selected = input$condition2_selected)
+    })
+    
+    output$sample_table <- renderDataTable(
+        reactive_sample_table() %>%
+            rownames_to_column(var = "sample") %>%
+            select(sample, everything())
+    )
+    
+    de_res_table <- reactive({
+        deseq(data = data,
+              condition1_name = input$condition1_name,
+              condition2_name = input$condition2_name,
+              condition1_selected = input$condition1_selected,
+              condition2_selected = input$condition2_selected,
+              gene_col = gene_col)
+    })
+    
+    observeEvent(input$apply, {
+        output$deseq_res_table <- renderDataTable(
+            de_res_table()
+        )
     })
 }
 
