1dda430c6394271ca21a86ff562819a768025051
lwolfe <lwolfe@fredhutch.org>
2021-09-08 13:24:56 -0700

    edger ma plot stuff

--- plot.R
+++ plot.R
@@ -14,7 +14,7 @@ resultsToMa <- function(results,
     pvalue_col <- ifelse(fdr, "padj", "pvalue")
 
   } else if (de_package == "edgeR") {
-    #mean_counts_col <- ??
+    mean_counts_col <- "logCPM"
     logfc_col <- "logFC"
     pvalue_col <- ifelse(fdr, "FDR", "PValue")
   }
@@ -27,10 +27,17 @@ resultsToMa <- function(results,
   de_idx <- factor(de_idx, levels = c(TRUE, FALSE))
   
   # plot
-  ggplot(results, aes(baseMean, log2FoldChange, colour = de_idx)) + 
-    geom_point(size=2, alpha = .75) + 
-    scale_y_continuous(limits=c(-3, 3), oob = squish) + 
-    scale_x_log10() + 
+  ma <- ggplot(results, aes(.data[[mean_counts_col]], .data[[logfc_col]], colour = de_idx)) +
+    geom_point(size=2, alpha = .7) + 
+    scale_y_continuous(limits=c(-3, 3), oob = squish)
+  
+  # if package is DESeq log10 the base mean axis
+  if (de_package == "DESeq2") {
+    ma <- ma +
+      scale_x_log10()
+  }
+  
+  ma +
     geom_hline(yintercept = 0, colour="grey", size=1) + 
     labs(x="mean of normalized counts", y="log fold change") + 
     theme_classic()
