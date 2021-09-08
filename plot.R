d65266aaa1b90ee738b2fea2f34d16abc1559edb
lwolfe <lwolfe@fredhutch.org>
2021-09-08 12:02:29 -0700

    ma plot function

---
+++ plot.R
@@ -0,0 +1,37 @@
+# plot de output in MA plot
+# basemean on x axis
+# log2foldchange on y axis
+resultsToMa <- function(results,
+                        de_package,
+                        pvalue_threshold,
+                        logfc_threshold,
+                        fdr) {
+  
+  # handle differing results table naming schemes
+  if (de_package == "DESeq2") {
+    mean_counts_col <- "baseMean"
+    logfc_col <- "log2FoldChange"
+    pvalue_col <- ifelse(fdr, "padj", "pvalue")
+
+  } else if (de_package == "edgeR") {
+    #mean_counts_col <- ??
+    logfc_col <- "logFC"
+    pvalue_col <- ifelse(fdr, "FDR", "PValue")
+  }
+  
+  # create boolean vector of significance
+  de_idx <- abs(results[[logfc_col]]) >= logfc_threshold & results[[pvalue_col]] <= pvalue_threshold
+  # NA to FALSE
+  de_idx[is.na(de_idx)] = FALSE
+  # Reset levels so TRUE is first
+  de_idx <- factor(de_idx, levels = c(TRUE, FALSE))
+  
+  # plot
+  ggplot(results, aes(baseMean, log2FoldChange, colour = de_idx)) + 
+    geom_point(size=2, alpha = .75) + 
+    scale_y_continuous(limits=c(-3, 3), oob = squish) + 
+    scale_x_log10() + 
+    geom_hline(yintercept = 0, colour="grey", size=1) + 
+    labs(x="mean of normalized counts", y="log fold change") + 
+    theme_classic()
+}
