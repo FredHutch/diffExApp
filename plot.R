124ee33d7c2300804df3ebef1c22e79817123fd9
lwolfe <lwolfe@fredhutch.org>
2021-09-23 16:28:37 -0700

    overhaul ma plot to be more similar to interactive volcano plot function, remove old volcano plotting function

--- plot.R
+++ plot.R
@@ -20,84 +20,51 @@ countsToPca <- function(de_out,
 
 ## RESULTS VISUALIZATIONS #########################################################################
 # plot de output in MA plot
-resultsToMa <- function(de_out,
-                        de_package,
-                        pvalue_threshold,
-                        logfc_threshold,
-                        fdr) {
-  # pull results from de output object
-  if (de_package == "DESeq2") {
-    de_res <- data.frame(results(de_out))
-  } else if (de_package == "edgeR") {
-    de_res <- data.frame(topTags(de_out, n = Inf))
-  }
+resultsToMa <- function(de_res,
+                        logfc_col,
+                        mean_counts_col,
+                        x_label,
+                        y_label,
+                        legend_title,
+                        de_vec) {
   
-  # create boolean vector of significance
-  de_idx <- abs(de_res[[logfc_col]]) >= logfc_threshold & de_res[[pvalue_col]] <= pvalue_threshold
-  # NA to FALSE
-  de_idx[is.na(de_idx)] = FALSE
-  # Reset levels so TRUE is first
-  de_idx <- factor(de_idx, levels = c(TRUE, FALSE))
+  # Reorder de_vec factors so that TRUE is first
+  de_vec <- factor(de_vec, levels=c("TRUE", "FALSE"))
   
-  # plot
-  ma <- ggplot(de_res, aes(.data[[mean_counts_col]], .data[[logfc_col]], colour = de_idx)) +
-    geom_point(size=2, alpha = .6) + 
+  # create axes names
+  axes <- NULL
+  axes$y <- ifelse(y_label == "",
+                   logfc_col,
+                   y_label)
+  axes$x <- ifelse(x_label == "",
+                   mean_counts_col,
+                   x_label)
+  
+  axes$x <- ifelse(legend_title == "",
+                   "Differentially Expressed",
+                   legend_title)
+  # base plot
+  ma <- ggplot(de_res, aes(.data[[mean_counts_col]], .data[[logfc_col]])) +
+    geom_point(size=2, alpha = .6, aes(colour = de_vec)) + 
     scale_y_continuous(limits=c(-3, 3), oob = squish)
   
   # if package is DESeq log10 the base mean axis
-  if (de_package == "DESeq2") {
+  # check for basemean col
+  basemean_present <- "baseMean" %in% names(de_res)
+  if (basemean_present) {
     ma <- ma +
       scale_x_log10()
   }
   
-  ma +
-    geom_hline(yintercept = 0, colour="grey", size=1, linetype = "dashed") + 
-    labs(x="Mean of Normalized Counts", y="Log Fold Change") + 
-    theme_classic()
+  # add hline
+  ma <- ma +
+    geom_hline(yintercept = 0, colour="grey", size=1, linetype = "dashed")
+  
+  # add custom axes and legend labels
+  ma <- ma +
+    labs(x = axes$x, y = axes$y, color = legend_title) +
+    theme_classic(base_size = 12)
+  
+  # display plot
+  ma
 }
-
-# # plot de results as volcano plot
-# resultsToVolcano <- function(de_out,
-#                              de_package,
-#                              pvalue_threshold,
-#                              logfc_threshold,
-#                              fdr) {
-#   # pull results from de results object
-#   if (de_package == "DESeq2") {
-#     de_res <- data.frame(results(de_out))
-#   } else if (de_package == "edgeR") {
-#     de_res <- data.frame(topTags(de_out, n = Inf))
-#   }
-#   
-#   # handle differing results table naming schemes
-#   if (de_package == "DESeq2") {
-#     logfc_col <- "log2FoldChange"
-#     pvalue_col <- ifelse(fdr, "padj", "pvalue")
-#     
-#   } else if (de_package == "edgeR") {
-#     logfc_col <- "logFC"
-#     pvalue_col <- ifelse(fdr, "FDR", "PValue")
-#   }
-#   
-#   # convert pval to -log10(pval)
-#   de_res <- mutate(de_res,
-#                  log_pval = -log10(de_res[[pvalue_col]]))
-#   
-#   de_idx <- abs(de_res[[logfc_col]]) >= logfc_threshold & de_res[[pvalue_col]] <= pvalue_threshold
-#   # NA to FALSE
-#   de_idx[is.na(de_idx)] = FALSE
-#   # Reset levels so TRUE is first
-#   de_idx <- factor(de_idx, levels = c(TRUE, FALSE))
-# 
-#   # build plot
-#   volcano <- ggplot(de_res, aes(x = .data[[logfc_col]], y = log_pval)) +
-#     geom_point(size = 2, alpha = .6, aes(color = de_idx)) +
-#     geom_hline(yintercept = -log10(pvalue_threshold), linetype = "dashed", col = "grey", size = 1) +
-#     geom_vline(xintercept = c(logfc_threshold, -logfc_threshold), linetype = "dashed", col = "grey", size = 1) +
-#     scale_y_continuous(limits=c(0, 10), oob = squish) +
-#     labs(x = "Log Fold Change", y = "-log10(P value)", color = "Is DE") +
-#     theme_classic(base_size = 12)
-#   
-#   volcano
-# 
-# }
